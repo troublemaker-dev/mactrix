c39902026f410770c41b6dd2ccacd2afdaa69498
viktorstrate <viktorstrate@gmail.com>
2025-10-30 13:41:25 +0100

    Initial commit

---
+++ Mactrix/MactrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MactrixApp.swift
+//  Mactrix
+//
+//  Created by Viktor Strate Kløvedal on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MactrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MactrixTests/MactrixTests.swift
@@ -0,0 +1,17 @@
+//
+//  MactrixTests.swift
+//  MactrixTests
+//
+//  Created by Viktor Strate Kløvedal on 30/10/2025.
+//
+
+import Testing
+@testable import Mactrix
+
+struct MactrixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
